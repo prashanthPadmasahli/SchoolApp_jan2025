e4f62bee92996c27f72df0b597da8f855542481a
prashanthPadmashali <prashanthkumarshettigar@gmail.com>
2025-01-23 21:40:43 +0530

    Initial Commit

---
+++ SchoolApp_jan2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SchoolApp_jan2025
+//
+//  Created by mac on 23/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SchoolApp_jan2025/SchoolApp_jan2025App.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolApp_jan2025App.swift
+//  SchoolApp_jan2025
+//
+//  Created by mac on 23/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SchoolApp_jan2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
